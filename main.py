b03d6ee40a457b6dd6d451a4a96fef7b989e86ca
ChrisCodes19 <foog15b@gmail.com>
2025-12-11 12:48:14 -0500

    added base shoot method

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 import sys
 
                  
@@ -18,9 +19,11 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     AsteroidField.containers = (updatable,)
     Asteroid.containers = (asteroids, updatable, drawable)
     Player.containers = (updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
     player = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
     while True:
@@ -39,12 +42,6 @@ def main():
             spr.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-        
-    
-    
-    
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}\n Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
